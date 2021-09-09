10c5cecedb1fa648bbe7b24680e05322220b2aae
sabinabernardes <sabina.ricardok@gmail.com>
2021-09-09 04:10:04 -0300

    app camera

---
+++ app/src/main/java/com/example/camera/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.camera
+
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import androidx.appcompat.view.menu.MenuView
+import androidx.core.view.drawToBitmap
+
+const val REQUEST_IMAGE_CAPTURE = 1
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<View>(R.id.button).setOnClickListener {
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+                takePictureIntent.resolveActivity(packageManager)?.also {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+                }
+            }
+        }
+    }
+}
